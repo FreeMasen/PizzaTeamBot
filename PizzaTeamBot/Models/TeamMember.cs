413704097bc80d0a7b60645a95b99b0149a698b4
Robert Masen <RMasen@pdastats.com>
2017-03-07 12:54:23 -0600

    init

---
+++ PizzaTeamBot/Models/TeamMember.cs
@@ -0,0 +1,23 @@
+﻿
+namespace PizzaTeamBot.Models
+{
+    public class TeamMemer
+    {
+        string Id;
+        string Name;
+        int NumberOfBeers;
+
+        private TeamMemer()
+        {
+
+        }
+
+        public TeamMemer(string id, string name, int beers)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.NumberOfBeers = beers;
+        }
+
+    }
+}
---
+++ PizzaTeamBot/Services/Standings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace PizzaTeamBot.Services
+{
+    public class Standings
+    {
+        private string ConnectionString; 
+
+        private Standings()
+        {
+
+        }
+
+        public void Increment(int Id)
+        {
+
+        }
+
+        public void GetStandings()
+        {
+
+        }
+    }
+}
